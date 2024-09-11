23ba9ecc71345d85ac8062c56ed22b403c855051
meirshoulkin <meirshoulkin@gmail.com>
2024-09-11 10:35:52 +0300

    ss57

--- src/test/java/config/ApplicationManager.java
+++ src/test/java/config/ApplicationManager.java
@@ -1,11 +1,14 @@
 package config;
 
+import dev.failsafe.internal.util.Durations;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
+
+//import java.time.Duration;
 
 public class ApplicationManager {
 
@@ -19,7 +22,7 @@ public class ApplicationManager {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
     }
 
     @AfterMethod
